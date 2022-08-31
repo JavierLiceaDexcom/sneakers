049a1074a12126ec36e8dbdffe5e03b0ff706e9f
Javier Mendoza <pacopancho413@gmail.com>
2022-08-30 19:32:29 -0500

    Add navigation to card and address form fragments

--- app/src/main/java/com/xavidev/testessential/ui/profile/AddressFormFragment.kt
+++ app/src/main/java/com/xavidev/testessential/ui/profile/AddressFormFragment.kt
@@ -2,19 +2,40 @@ package com.xavidev.testessential.ui.profile
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.viewModels
+import com.xavidev.testessential.R
 import com.xavidev.testessential.databinding.FragmentAddressFormBinding
 
 
-class AddressFormFragment : Fragment() {
+class AddressFormFragment : DialogFragment() {
 
     private val binding by lazy(LazyThreadSafetyMode.NONE) {
         FragmentAddressFormBinding.inflate(layoutInflater)
     }
 
+    private val viewModel: AddressViewModel by viewModels()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ) = binding.root
+
+    override fun getTheme() = R.style.FullscreenDialogTheme_Primary
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        dialog?.window?.attributes?.windowAnimations = R.style.DialogAnimation
+
+        binding.apply {
+            lifecycleOwner = viewLifecycleOwner
+            vm = viewModel
+        }
+
+        binding.tbrAddressForm.setNavigationOnClickListener {
+            viewModel.navigateTo(view, R.id.action_addressFormFragment_to_addressesFragment)
+        }
+    }
 }
--- app/src/main/java/com/xavidev/testessential/ui/profile/CardFormFragment.kt
+++ app/src/main/java/com/xavidev/testessential/ui/profile/CardFormFragment.kt
@@ -2,17 +2,38 @@ package com.xavidev.testessential.ui.profile
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.viewModels
+import com.xavidev.testessential.R
 import com.xavidev.testessential.databinding.FragmentCardFormBinding
 
-class CardFormFragment : Fragment() {
+class CardFormFragment : DialogFragment() {
     val binding by lazy(LazyThreadSafetyMode.NONE) {
         FragmentCardFormBinding.inflate(layoutInflater)
     }
+
+    private val viewModel: PaymentMethodViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ) = binding.root
+
+    override fun getTheme() = R.style.FullscreenDialogTheme_Primary
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        dialog?.window?.attributes?.windowAnimations = R.style.DialogAnimation
+
+        binding.apply {
+            lifecycleOwner = viewLifecycleOwner
+            vm = viewModel
+        }
+
+        binding.tbrAddPayment.setNavigationOnClickListener {
+            viewModel.navigateTo(view, R.id.action_cardFormFragment_to_paymentMethodsFragment)
+        }
+    }
 }
