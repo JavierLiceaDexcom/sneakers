75fa828a24b8c8085714dda4c2a87cdb344e4962
Javier Mendoza <pacopancho413@gmail.com>
2022-09-02 00:53:39 -0500

    Create cart viewmodel

--- app/src/main/java/com/xavidev/testessential/ui/cart/CartFragment.kt
+++ app/src/main/java/com/xavidev/testessential/ui/cart/CartFragment.kt
@@ -2,8 +2,10 @@ package com.xavidev.testessential.ui.cart
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import com.xavidev.testessential.databinding.FragmentCartBinding
 
 class CartFragment : Fragment() {
@@ -12,8 +14,19 @@ class CartFragment : Fragment() {
         FragmentCartBinding.inflate(layoutInflater)
     }
 
+    private val viewModel: CartViewModel by activityViewModels { CartViewModel.Factory() }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ) = binding.root
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+            lifecycleOwner = viewLifecycleOwner
+            vm = viewModel
+        }
+    }
 }
--- app/src/main/java/com/xavidev/testessential/ui/cart/CartViewModel.kt
+++ app/src/main/java/com/xavidev/testessential/ui/cart/CartViewModel.kt
@@ -1,6 +1,87 @@
 package com.xavidev.testessential.ui.cart
 
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
+import com.xavidev.testessential.data.State
+import com.xavidev.testessential.data.dao.Action
+import com.xavidev.testessential.data.db.DatabaseManager
+import com.xavidev.testessential.data.entity.Cart
+import com.xavidev.testessential.repository.CartRepository
+import com.xavidev.testessential.resources.CartResources
+import com.xavidev.testessential.utils.NavigationViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.launch
 
-class CartViewModel: ViewModel() {
+class CartViewModel(private val cartRepository: CartRepository) : NavigationViewModel() {
+
+    private val _loadingInsert = MutableLiveData(false)
+    val loadingInsert: LiveData<Boolean> get() = _loadingInsert
+
+    private val _loadingDelete = MutableLiveData(false)
+    val loadingDelete: LiveData<Boolean> get() = _loadingDelete
+
+    private val _loadingCartItems = MutableLiveData(false)
+    val loadingCartItems: LiveData<Boolean> get() = _loadingCartItems
+
+    private val _loadingCartItemUpdate = MutableLiveData(false)
+    val loadingCartItemUpdate: LiveData<Boolean> get() = _loadingCartItemUpdate
+
+    private val _cartItems = MutableLiveData<List<Cart>>()
+    val cartItems: LiveData<List<Cart>> get() = _cartItems
+
+    fun insertCartItem(cart: Cart) = viewModelScope.launch {
+        cartRepository.insertCartItem(cart).flowOn(Dispatchers.IO)
+            .collect { response ->
+                when (response.status!!) {
+                    State.LOADING -> _loadingInsert.postValue(true)
+                    State.SUCCESS -> _loadingInsert.postValue(false)
+                    State.ERROR -> _loadingInsert.postValue(false)
+                }
+            }
+    }
+
+    fun deleteCardItem(cart: Cart) = viewModelScope.launch {
+        cartRepository.deleteCartItem(cart).flowOn(Dispatchers.IO)
+            .collect { response ->
+                when (response.status!!) {
+                    State.LOADING -> _loadingDelete.postValue(true)
+                    State.SUCCESS -> _loadingDelete.postValue(false)
+                    State.ERROR -> _loadingDelete.postValue(false)
+                }
+            }
+    }
+
+    fun getCartItems() = viewModelScope.launch {
+        cartRepository.getCartItems().flowOn(Dispatchers.IO)
+            .collect { response ->
+                when (response.status!!) {
+                    State.LOADING -> _loadingCartItems.postValue(true)
+                    State.SUCCESS -> {
+                        _loadingCartItems.postValue(false)
+                        _cartItems.postValue(response.data ?: listOf())
+                    }
+                    State.ERROR -> _loadingCartItems.postValue(false)
+                }
+            }
+    }
+
+    fun updateCartItemQuantity(cartId: String, action: Action) = viewModelScope.launch {
+        cartRepository.updateCartItemQuantity(cartId, action).flowOn(Dispatchers.IO)
+            .collect { response ->
+                when (response.status!!) {
+                    State.LOADING -> _loadingCartItemUpdate.postValue(true)
+                    State.SUCCESS -> _loadingCartItemUpdate.postValue(false)
+                    State.ERROR -> _loadingCartItemUpdate.postValue(false)
+                }
+            }
+    }
+
+    class Factory() : ViewModelProvider.Factory {
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(CartViewModel::class.java)) {
+                return CartResources(DatabaseManager.instance.database.cartDao()) as T
+            }
+            throw Exception("No class supported")
+        }
+    }
 }
