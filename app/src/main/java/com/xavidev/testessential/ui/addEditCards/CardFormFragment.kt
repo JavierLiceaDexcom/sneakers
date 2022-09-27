f8fd55597d896bed0778c6d2086f035ffa5794fa
Javier Licea <francisco.mendoza@dexcom.com>
2022-09-27 15:04:33 -0500

    Add banking institution dropdown list to form

--- app/src/main/java/com/xavidev/testessential/ui/addEditCards/CardFormFragment.kt
+++ app/src/main/java/com/xavidev/testessential/ui/addEditCards/CardFormFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.viewModels
 import com.wajahatkarim3.easyvalidation.core.view_ktx.validator
@@ -12,7 +13,6 @@ import com.xavidev.testessential.R
 import com.xavidev.testessential.SneakersApplication
 import com.xavidev.testessential.data.source.local.entity.Card
 import com.xavidev.testessential.databinding.FragmentCardFormBinding
-import com.xavidev.testessential.ui.paymentMethods.PaymentMethodViewModel
 import com.xavidev.testessential.utils.*
 
 class CardFormFragment : DialogFragment() {
@@ -58,8 +58,21 @@ class CardFormFragment : DialogFragment() {
         binding.tbrAddPayment.setNavigationOnClickListener {
             showConfirmationDialog()
         }
-
+        setupForm()
         handleObservers()
+    }
+
+    private fun setupForm() {
+        val bankingInstitutionsAdapter = ArrayAdapter(
+            requireContext(),
+            android.R.layout.simple_expandable_list_item_1,
+            resources.getStringArray(R.array.banks)
+        )
+        binding.atvBankingInstitution.setAdapter(bankingInstitutionsAdapter)
+
+        binding.atvBankingInstitution.setOnFocusChangeListener { _, focused ->
+            if (focused) binding.atvBankingInstitution.showDropDown()
+        }
     }
 
     private fun handleObservers() {
@@ -84,9 +97,10 @@ class CardFormFragment : DialogFragment() {
         val expirationDate = binding.etExpirationDate.text.toString()
         val cvv = binding.etCvv.text.toString().toInt()
         val isDefault = binding.cbxDefaultPayment.isChecked
+        val bankingInstitution = binding.atvBankingInstitution.text.toString()
 
         return Card(
-            institutionName = "BBVA",
+            institutionName = bankingInstitution,
             cardNumber = cardNumber,
             expirationDate = expirationDate,
             ownerName = beneficiary,
@@ -117,7 +131,11 @@ class CardFormFragment : DialogFragment() {
             .addErrorCallback { binding.etCvv.error = requiredFieldMessage }
             .addSuccessCallback { binding.etCvv.error = null }.check()
 
-        return isCardNumberValid && isBeneficiaryValid && isExpirationValid && isCVVValid
+        val isInstitutionValid = binding.atvBankingInstitution.validator().nonEmpty()
+            .addErrorCallback { binding.atvBankingInstitution.error = requiredFieldMessage }
+            .addSuccessCallback { binding.atvBankingInstitution.error = null }.check()
+
+        return isInstitutionValid && isCardNumberValid && isBeneficiaryValid && isExpirationValid && isCVVValid
     }
 
     private fun showConfirmationDialog() {
